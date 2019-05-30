5c38617c905244ce50813ad01a924683257475db
christopherchoe <chchchoe@gmail.com>
2019-05-30 16:17:24 +0000

    changed limit number also

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,3 @@
 exec { 'modify_file':
-  command => '/bin/echo "ULIMIT=\"-n 1500\"" | sudo tee /etc/default/nginx && sudo /usr/bin/service nginx restart',
+  command => '/bin/echo "ULIMIT=\"-n 7000\"" | sudo tee /etc/default/nginx && sudo /usr/bin/service nginx restart',
 }
