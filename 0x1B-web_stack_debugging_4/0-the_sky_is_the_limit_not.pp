d75055bfbcad40ca36c8669fcf2f24a19309200a
christopherchoe <chchchoe@gmail.com>
2019-05-30 15:21:36 +0000

    made more universal

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,6 @@ service { 'nginx':
 }
 
 exec { 'modify_file':
-  command => "/bin/sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 1500\"/g' /etc/default/nginx",
+  command => "/bin/sed -i 's/ULIMIT=\"-n /ULIMIT=\"-n 150/g' /etc/default/nginx",
   notify  => Service['nginx']
 }
-
